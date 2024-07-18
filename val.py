a6e5437f18792962ffa661572a06098758ca4425
wenbin-zheng <zwbben@qq.com>
2024-07-18 15:25:07 +0800

    val.py

--- val.py
+++ val.py
@@ -1,9 +1,9 @@
 import warnings
 warnings.filterwarnings('ignore')
-from wddetr import RTDETR
+from wddetr import WeedDETR
 
 if __name__ == '__main__':
-    model = RTDETR('weed-detr.pt')
+    model = WeedDETR('weed-detr.pt')
     model.val(data='data.yaml',
               split='test',
               imgsz=640,
