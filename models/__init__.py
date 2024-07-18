7c815f1ff62bc8448289d5fa799b3e517e7d8ee6
wenbin-zheng <zwbben@qq.com>
2024-07-18 16:05:07 +0800

    __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 
 
-from .rtdetr import RTDETR
+from .rtdetr import RTDETR as WeedDETR
 
 from .yolo import YOLO
 
-__all__ = 'YOLO', 'RTDETR',  # allow simpler import
+__all__ = 'YOLO', 'WeedDETR',  # allow simpler import
